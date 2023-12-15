54ec5f3f3d948e4f8fcf113835f369e348f8a41e
WavyBoyJodii <najeecasey@gmail.com>
2023-12-15 04:25:47 -0500

    create add friend insert functions and schema into 1 file

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,7 +9,8 @@ import {
   pgEnum,
   uniqueIndex,
 } from 'drizzle-orm/pg-core';
-import { sql } from 'drizzle-orm';
+import { sql, eq, and, relations } from 'drizzle-orm';
+import { db } from './db';
 
 export const friendStatusEnum = pgEnum('status', [
   'pending',
@@ -94,3 +95,86 @@ export type NewFriendsType = typeof Friends.$inferInsert;
 //     };
 //   }
 // );
+
+export const userRelations = relations(User, ({ many }) => ({
+  message: many(Message),
+  chat: many(Chat),
+  friends: many(Friends),
+}));
+
+export const messageRelations = relations(Message, ({ one }) => ({
+  user: one(User, {
+    fields: [Message.user_id],
+    references: [User.id],
+  }),
+  chat: one(Chat, {
+    fields: [Message.chat_id],
+    references: [Chat.id],
+  }),
+}));
+
+export const friendRelations = relations(Friends, ({ one }) => ({
+  user: one(User, {
+    fields: [Friends.user_id1],
+    references: [User.id],
+  }),
+  friend: one(User, {
+    fields: [Friends.user_id2],
+    references: [User.id],
+  }),
+}));
+
+export const chatRelations = relations(Chat, ({ one, many }) => ({
+  message: many(Message),
+  user1: one(User, {
+    fields: [Chat.user_id1],
+    references: [User.id],
+  }),
+  user2: one(User, {
+    fields: [Chat.user_id2],
+    references: [User.id],
+  }),
+}));
+
+export const getUserByUsername = async (username: string) => {
+  const result = await db
+    .select({
+      id: User.id,
+      username: User.username,
+      email: User.email,
+      firstName: User.first_name,
+      lastName: User.last_name,
+    })
+    .from(User)
+    .where(eq(User.username, username));
+  if (result.length === 0) return null;
+  const user = result[0];
+  return user;
+};
+export const getUserById = async (id: number) => {
+  const result = await db.select().from(User).where(eq(User.id, id));
+  if (result.length === 0) return null;
+  const user = result[0];
+  return user;
+};
+
+export const createUser = async (newUser: NewUserType) => {
+  const result = await db.insert(User).values(newUser).returning();
+  return result;
+};
+
+export const getFriendsList = async (id: string) => {
+  const idNumber = Number(id);
+  const result = await db.query.Friends.findMany({
+    where: and(eq(Friends.user_id1, idNumber), eq(Friends.status, 'accepted')),
+    with: {
+      friend: true,
+    },
+  });
+  return result;
+};
+
+export const addFriend = async (newFriend: NewFriendsType) => {
+  const result = await db.insert(Friends).values(newFriend).returning();
+  return result;
+};
