7115aa2e3e45d37fa0714e19a32bc23df1ad14c7
WavyBoyJodii <najeecasey@gmail.com>
2023-12-15 06:25:06 -0500

    migrate functions to own file to fix sql generation issue

--- src/db/functions.ts
+++ src/db/functions.ts
@@ -7,48 +7,107 @@ import {
   NewMessageType,
   NewUserType,
 } from './schema';
-import { eq, and } from 'drizzle-orm';
+import { eq, and, relations } from 'drizzle-orm';
 import { db } from './db';
 
-// export const getUserByUsername = async (username: string) => {
-//   const result = await db
-//     .select({
-//       id: User.id,
-//       username: User.username,
-//       email: User.email,
-//       firstName: User.first_name,
-//       lastName: User.last_name,
-//     })
-//     .from(User)
-//     .where(eq(User.username, username));
-//   if (result.length === 0) return null;
-//   const user = result[0];
-//   return user;
-// };
-// export const getUserById = async (id: number) => {
-//   const result = await db.select().from(User).where(eq(User.id, id));
-//   if (result.length === 0) return null;
-//   const user = result[0];
-//   return user;
-// };
+export const getUserByUsername = async (username: string) => {
+  const result = await db
+    .select({
+      id: User.id,
+      username: User.username,
+      email: User.email,
+      firstName: User.first_name,
+      lastName: User.last_name,
+    })
+    .from(User)
+    .where(eq(User.username, username));
+  if (result.length === 0) return null;
+  const user = result[0];
+  return user;
+};
+export const getUserById = async (id: number) => {
+  const result = await db.select().from(User).where(eq(User.id, id));
+  if (result.length === 0) return null;
+  const user = result[0];
+  return user;
+};
 
-// export const createUser = async (newUser: NewUserType) => {
-//   const result = await db.insert(User).values(newUser).returning();
-//   return result;
-// };
+export const createUser = async (newUser: NewUserType) => {
+  const result = await db.insert(User).values(newUser).returning();
+  return result;
+};
 
-// export const getFriendsList = async (id: string) => {
-//   const idNumber = Number(id);
-//   const result = await db.query.Friends.findMany({
-//     where: and(eq(Friends.user_id1, idNumber), eq(Friends.status, 'accepted')),
-//     with: {
-//       friend: true,
-//     },
-//   });
-//   return result;
-// };
+export const checkFriendship = async (myId: number, friendId: number) => {
+  const result = await db.query.Friends.findFirst({
+    where: and(
+      eq(Friends.user_id1, myId),
+      eq(Friends.user_id2, friendId),
+      eq(Friends.status, 'accepted')
+    ),
+    with: {
+      friend: true,
+    },
+  });
+  return result;
+};
 
-// export const addFriend = async (newFriend: NewFriendsType) => {
-//   const result = await db.insert(Friends).values(newFriend).returning();
-//   return result;
-// };
+export const getFriendsList = async (id: string) => {
+  const idNumber = Number(id);
+  const result = await db.query.Friends.findMany({
+    where: and(eq(Friends.user_id1, idNumber), eq(Friends.status, 'accepted')),
+    with: {
+      friend: true,
+    },
+  });
+  return result;
+};
+
+export const getFriendRequests = async (id: string) => {
+  const idNumber = Number(id);
+  const result = await db.query.Friends.findMany({
+    where: and(eq(Friends.user_id1, idNumber), eq(Friends.status, 'pending')),
+    with: {
+      friend: true,
+    },
+  });
+  return result;
+};
+
+export const addFriend = async (newFriend: NewFriendsType) => {
+  const result = await db.insert(Friends).values(newFriend).returning();
+  return result;
+};
+
+export const acceptFriend = async (myId: number, friendId: number) => {
+  const newFriend = await db
+    .update(Friends)
+    .set({ status: 'accepted' })
+    .where(and(eq(Friends.user_id1, myId), eq(Friends.user_id2, friendId)))
+    .returning();
+
+  return newFriend;
+};
+
+export const deleteFriend = async (myId: number, friendId: number) => {
+  const deletedFriendId = await db
+    .delete(Friends)
+    .where(and(eq(Friends.user_id1, myId), eq(Friends.user_id2, friendId)))
+    .returning({ deletedId: Friends.user_id2 });
+  return deletedFriendId;
+};
+
+export const createChat = async (myId: number, friendId: number) => {
+  const result = await db
+    .insert(Chat)
+    .values({ user_id1: myId, user_id2: friendId })
+    .returning();
+  return result;
+};
+
+export const deleteChat = async (myId: number, friendId: number) => {
+  const result = await db
+    .delete(Chat)
+    .where(and(eq(Chat.user_id1, myId), eq(Chat.user_id2, friendId)))
+    .returning({ deletedId: Chat.id });
+  return result;
+};
