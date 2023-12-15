a829b449c8742dc1424a104781fabb1e73fa38d3
WavyBoyJodii <najeecasey@gmail.com>
2023-12-15 09:49:57 -0500

    update drizzle config for railway

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,9 +7,9 @@ export default {
   out: './drizzle/migrations',
   driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME!,
+    host: process.env.PGHOST!,
+    user: process.env.PGUSER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB!,
   },
 } satisfies Config;
