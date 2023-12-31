546bc9e4ca0285b8bcd6dd98cd4a79d10455bb75
WavyBoyJodii <najeecasey@gmail.com>
2023-12-31 09:58:13 -0500

    add pusher trigger to viewmychats

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -148,6 +148,9 @@ export const viewMyChats = expressAsyncHandler(
     if (!chats) {
       res.status(500).json({ message: `req.params.id = ${req.params.id}` });
     } else {
+      pusher.trigger(`chats-${req.params.id}`, 'mychats', {
+        chats,
+      });
       res.status(200).json(chats);
     }
   }
